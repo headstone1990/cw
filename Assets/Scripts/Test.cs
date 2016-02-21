7d13c1c4f8d5736363c95fdd26b414f453a800d6
HeadStone <feuerschwanz1990@gmail.com>
2016-02-22 00:30:08 +0300

    Restore Test.cs

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -23,25 +23,31 @@ public class Test : MonoBehaviour
 
         for (int i = 0; i < pix.Length; i++)
         {
-            if (pix2[i].a < pix[i].a)
-            {
-                pixResult[i] = pix[i];
-            }
-            else
-            {
-                pixResult[i] = pix2[i];
-            }
+            pixResult[i] = AlphaBlend(pix2[i], pix[i]);
         }
 
         Texture2D result = new Texture2D(1000, 600, TextureFormat.ARGB32, false);
         result.SetPixels(pixResult);
         result.Apply();
 
+
         //var c = ImageAsset1.bytes.Concat(ImageAsset2.bytes).ToArray();
         //tex.LoadRawTextureData(c);
         _spite1 = Sprite.Create(result, new Rect(0, 0, 1000, 600), new Vector2(0, 0));
         Image.sprite = _spite1;
 
+        
+
+    }
+
+    float BlendSubpixel(float top, float bottom, float alphaTop, float alphaBottom)
+    {
+        return (top*alphaTop + bottom*alphaBottom*(1 - alphaTop))/(alphaTop + alphaBottom*(1 - alphaTop));
+    }
+
+    Color AlphaBlend(Color top, Color bottom)
+    {
+        return new Color(BlendSubpixel(top.r, bottom.r ,top.a, bottom.a), BlendSubpixel(top.g, bottom.g, top.a, bottom.a), BlendSubpixel(top.b, bottom.b, top.a, bottom.a), top.a + bottom.a * (1 - top.a));
     }
 
 }
