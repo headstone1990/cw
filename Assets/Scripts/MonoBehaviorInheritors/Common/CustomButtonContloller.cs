93f055c85333086ccad21a82ced15802bfb19e0b
HeadStone <feuerschwanz1990@gmail.com>
2016-04-08 17:07:12 +0300

    Add CustomButtonContloller

---
+++ Assets/Scripts/MonoBehaviorInheritors/Common/CustomButtonContloller.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace MonoBehaviorInh.Common
+{
+    [RequireComponent(typeof(AudioSource))]
+    [RequireComponent(typeof(Animator))]
+    public class CustomButtonContloller : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, ISelectHandler, IDeselectHandler, IUpdateSelectedHandler {
+        private Animator _animator;
+        private AudioSource _audioSource;
+        private bool _isPointerDown;
+        [SerializeField]
+        private UnityEvent _onPointerClick = null; // Set in inspector
+        [SerializeField]
+        private AudioClip _enter = null; // Set in inspector
+        [SerializeField]
+        private AudioClip _down = null; // Set in inspector
+
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _animator.SetBool("IsMouseEnter", true);
+            if (_isPointerDown == false)
+            {
+                _audioSource.PlayOneShot(_enter);
+            }
+
+        }
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _animator.SetBool("IsMouseEnter", false);
+
+        }
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _animator.SetBool("isMouseDown", true);
+            _isPointerDown = true;
+            _audioSource.PlayOneShot(_down);
+        }
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _animator.SetBool("isMouseDown", false);
+            _isPointerDown = false;
+            _onPointerClick.Invoke();
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            _animator.SetBool("IsMouseEnter", true);
+            if (_isPointerDown == false)
+            {
+                _audioSource.PlayOneShot(_enter);
+            }
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            _animator.SetBool("IsMouseEnter", false);
+        }
+
+        public void OnUpdateSelected(BaseEventData eventData)
+        {
+            if (Input.GetButtonDown("Submit"))
+            {
+                _animator.SetBool("isMouseDown", true);
+                _isPointerDown = true;
+                _audioSource.PlayOneShot(_down);
+            }
+            if (Input.GetButtonUp("Submit"))
+            {
+                _animator.SetBool("isMouseDown", false);
+                _isPointerDown = false;
+                _onPointerClick.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MonoBehaviorInheritors/Common/GameObjectSelector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+namespace MonoBehaviorInh.Common
+{
+    public class GameObjectSelector : MonoBehaviour
+    {
+        [SerializeField] EventSystem _eventSystem;
+
+        private void Update()
+        {
+            if (_eventSystem.currentSelectedGameObject == null)
+            {
+                if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical"))
+                {
+                    _eventSystem.SetSelectedGameObject(_eventSystem.firstSelectedGameObject);
+                }
+
+            }
+        }
+    }
+}
